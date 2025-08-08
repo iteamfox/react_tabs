e654c1d6dd695dd2c001d7a930ea50f44bca93f7
iteamfox <avlisunenko@gmail.com>
2025-08-08 23:35:22 +0200

    Add task v2

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,36 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
 import { Tabs } from './components/Tabs';
 
-type Tab = {
+export type Tab = {
   id: string;
   title: string;
   content: string;
 };
 
-export const tabs = [
+export const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
 export const App: React.FC = () => {
-  const [selectedTabId, setSelectedTabId] = useState('tab-1');
+  const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId)!;
 
-  const onTabSelected = (event: React.MouseEvent, tab: Tab) => {
-    event?.preventDefault();
-
-    setSelectedTabId(tab.id);
+  const handleTabSelect = (tab: Tab) => {
+    if (tab.id !== selectedTabId) {
+      setSelectedTabId(tab.id);
+    }
   };
 
   return (
     <div className="section">
+      <h1 className="title">Selected tab is {selectedTab.title}</h1>
+
       <Tabs
         tabs={tabs}
         selectedTabId={selectedTabId}
-        onTabSelected={onTabSelected}
+        onTabSelected={handleTabSelect}
       />
     </div>
   );
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,52 +1,47 @@
 import React from 'react';
+import type { Tab } from '../../App';
 
-type Tab = {
-  id: string;
-  title: string;
-  content: string;
-};
-
-type Props = {
+type TabsProps = {
   tabs: Tab[];
   selectedTabId: string;
-  onTabSelected: (event: React.MouseEvent, tab: Tab) => void;
+  onTabSelected: (tab: Tab) => void;
 };
 
-export const Tabs = ({ tabs, selectedTabId, onTabSelected }: Props) => {
+export const Tabs: React.FC<TabsProps> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
   return (
-    <>
-      <h1 className="title">
-        Selected tab is {tabs.find(tab => tab.id === selectedTabId)?.title}
-      </h1>
-      <div data-cy="TabsComponent">
-        <div className="tabs is-boxed">
-          <ul>
-            {tabs.map(tab => {
-              return (
-                <li
-                  key={tab.id}
-                  className={tab.id === selectedTabId ? 'is-active' : ''}
-                  data-cy="Tab"
-                >
-                  <a
-                    href={tab.id}
-                    data-cy="TabLink"
-                    onClick={(event: React.MouseEvent) =>
-                      onTabSelected(event, tab)
-                    }
-                  >
-                    {tab.title}
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-
-        <div className="block" data-cy="TabContent">
-          {tabs.find(tab => tab.id === selectedTabId)?.content}
-        </div>
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              className={tab.id === selectedTabId ? 'is-active' : ''}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={event => {
+                  event.preventDefault();
+                  if (tab.id !== selectedTabId) {
+                    onTabSelected(tab);
+                  }
+                }}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
-    </>
+
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === selectedTabId)?.content}
+      </div>
+    </div>
   );
 };
